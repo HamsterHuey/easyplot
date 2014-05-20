d83e7d4983e24f2f500f4f1e336e33ec4c990393
Sudeep Mandal <sudeepmandal@gmail.com>
2014-05-20 01:38:48 -0400

    Restructure folder layout and create __init__.py

---
+++ easyplot/__init__.py
@@ -0,0 +1,8 @@
+# Sudeep Mandal 05/20/2014
+# EasyPlot - A Matplotlib convenience wrapper for fast and easy creation of
+# reusable plots
+
+"""
+For documentation read the README file
+"""
+from .easyplot import EasyPlot
